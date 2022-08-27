80d907de9c1e7f1dd5aceec721d944cd92b299da
codemacabre <49149591+codemacabre@users.noreply.github.com>
2022-08-27 13:43:40 +0100

    ♻️ Refactor to use Vue + Vite

---
+++ src/components/TheSummary.vue
@@ -0,0 +1,39 @@
+<script>
+import MissionObject from '../components/MissionObject.vue'
+import dataset from '../assets/data/mars-missions.json'
+
+export default {
+  data() {
+    return {
+      dataset
+    }
+  },
+  components: {
+    MissionObject
+  },
+  computed: {
+    operationalObjects() {
+      return this.dataset.filter(object => object.Outcome.text === 'Operational')
+    }
+  }
+}
+</script>
+
+<template>
+<section class="count">
+  <div id="summary">
+
+    
+    <h2>{{ operationalObjects.length }}</h2>
+    <span>operational robots are at Mars today</span>
+    <p>(still no humans)</p>
+  </div>
+</section>
+<section class="infopanel">
+  <MissionObject
+    class="item"
+    v-for="object in operationalObjects"
+    :data="object"
+  />
+</section>
+</template>
