bd4261bf18e53badd414bd79b2f1fab92caefcfb
codemacabre <49149591+codemacabre@users.noreply.github.com>
2022-08-27 13:52:37 +0100

    ✅ Add initial tests

---
+++ src/components/__tests__/TheSummary.test.js
@@ -0,0 +1,10 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import TheSummary from '../TheSummary.vue'
+
+describe('TheSummary', () => {
+  it('renders properly', () => {
+    // TODO
+  })
+})
